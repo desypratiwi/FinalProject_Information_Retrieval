9c0bd98101fa0ef4282ec463f640c8cdb9d70910
desypratiwi <pratiwidesyy48@gmail.com>
2018-12-07 17:09:27 +0700

    membuat class penyakit & method tokenizing

--- Penyakit.php
+++ Penyakit.php
@@ -1,3 +1,12 @@
 <?php
-	
+	class Penyakit{
+		$id_penyakit=-1;
+		$judul='';
+		$deskripsi = '';
+		$deskripsi_text = '';
+		$penyakit = '';
+		$url_page = '';
+		$url_page_ori = '';
+		$slug = '';
+	}
 ?>
