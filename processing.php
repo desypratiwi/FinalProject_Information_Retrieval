075b96eb677c01de6381b78b7ddba568b03105c4
nadyarizky <nadyaryota@gmail.com>
2018-12-09 12:02:45 +0700

    Menambahkan fungsi untuk merubah huruf menjadi kecil

--- processing.php
+++ processing.php
@@ -1,17 +1,25 @@
 <?php 
 include 'koneksi.php';
-$sql = "SELECT * from tb_mst_penyakit";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        print_r ($row);
-    }
-} else {
-    echo "0 results";
+function seluruh_data_db(){
+	
+	$sql = "SELECT * from tb_mst_penyakit";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) {
+		// output data of each row
+		while($row = mysqli_fetch_assoc($result)) {
+			print_r ($row);
+		}
+	} else {
+		echo "0 results";
+	}
+
+	mysqli_close($conn);
+
+//print_r ($result);
 }
 
-mysqli_close($conn);
-
-print_r ($result);
+function casefolding($str){
+	  
+	  return strtolower($str);
+}
 ?>
