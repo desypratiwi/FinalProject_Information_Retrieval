99487d36eae1f83a822392a8ed38a5304f674829
nadyarizky <nadyaryota@gmail.com>
2019-01-17 01:29:59 +0700

    Membuat file Keluhan, form keluhan dan datatable

---
+++ Keluhan.php
@@ -0,0 +1,50 @@
+<html>
+<script src="https://code.jquery.com/jquery-3.3.1.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js" type="text/javascript"></script>
+<link href='https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
+<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
+
+
+<form method="post" action="">
+	
+        <table>
+            <tr>
+                <td><textarea name="keluhan"><?php echo isset($_POST['keluhan'])?$_POST['keluhan']:"" ?></textarea></td>
+            </tr>
+            <tr>
+                <td><button type="submit">Deteksi</button></td>
+            </tr>
+        </table>
+        
+        
+    </form>
+    <table id="tb_hasil" <?php echo isset($_POST['keluhan'])?"" : "style='display:none;'" ?>  class="table table-striped table-bordered" style="width:100%" >
+        <thead>
+        <tr>
+            <th>No</th>
+            <th>Penyakit</th>
+            <th>Kecocokan</th>
+            <th>Detail</th>
+        </tr>
+        </thead>
+        <?php for($i=0;$i<count($data);$i++) {?>
+        <tr>
+            <td><?php echo ($i+1); ?></td>
+            <td><?php echo ($data[$i]->penyakit); ?></td>
+            <td><?php echo ($data[$i]->cosine_similarity); ?></td>
+            <td><a href="<?php echo ($data[$i]->url_page_ori); ?>"><button>Detail</button></a></td>
+        </tr>
+        <?php } ?>
+        
+    </table>
+</html>
+<script>
+    $(document).ready(function() {
+        $('#tb_hasil').DataTable({
+            "order": [[ 2, "desc" ]]
+        });
+         
+    } );
+</script>
+
